a433f218d932a462318aeec90fe49190069efc54
Sarah Thompson <sarahloisthompson@gmail.com>
2020-09-05 09:19:19 -0500

    first commit

---
+++ boredinchicago/boredinchicago/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  boredinchicago
+//
+//  Created by Sarah Thompson on 9/5/20.
+//  Copyright © 2020 SarahThompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
